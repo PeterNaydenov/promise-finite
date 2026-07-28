654a9251278a8898c175c22d8b9ab38c3cfbd513
Peter Naydenov <peter.naydenov@gmail.com>
2026-07-28 21:53:04 +0300

    Test: convert mocha test to vitest

--- test/01.OriginalPromise.js
+++ test/01.OriginalPromise.js
@@ -1,44 +1,25 @@
-'use strict';
-
-import { expect } from 'chai'
+import { describe, it, expect } from 'vitest'
 import { standard as promiseFinite } from '../index.js'
 
 
 
 describe ( 'Original Promise Library', () => {
 
-it ( 'Promise is resolved', done => {
-
-    promiseFinite ( 1000, 'expired', (resolve,reject) => {
+it ( 'Promise is resolved', async () => {
+    const x = await promiseFinite ( 1000, 'expired', (resolve,reject) => {
                         resolve('success')
                     })
-              .then ( x => {
-                                expect (x).is.equal ('success', 'Promise was not resolved');
-                                done()
-                    })
-            .catch ( err => {
-                         console.log (err)
-                         done ()
-                    })
+    expect (x).toBe ('success')
  }) // it resolved
 
 
 
-it ( 'Promise is timeouted', done => {
-    promiseFinite ( 500, 'timeout', (resolve, reject) => {
+it ( 'Promise is timeouted', async () => {
+    const x = await promiseFinite ( 500, 'timeout', (resolve, reject) => {
                         // ... forget to resolve should trigger the timeout
                   })
-            .then ( x => {
-                    expect (x).to.be.equal ('timeout', 'Promise was not timeouted');
-                    done()
-                  })
-          .catch ( err => {
-                         console.log (err)
-                         done ()
-                    })
+    expect (x).toBe ('timeout')
 }) // it not resolved
 
 
 }) // describe
-
-
--- test/02.BluebirdPromise.js
+++ test/02.BluebirdPromise.js
@@ -1,6 +1,4 @@
-'use strict';
-
-import { expect } from 'chai'
+import { describe, it, expect } from 'vitest'
 import bluebird from 'bluebird'
 import { customize as pfCustomize } from '../index.js'
 
@@ -10,35 +8,19 @@ const promiseFinite = pfCustomize(bluebird);
 
 describe ( 'Bluebird Promise Library', () => {
 
-it ( 'Promise is resolved', done => {
-    promiseFinite ( 1000, 'expired', (resolve,reject) => {
+it ( 'Promise is resolved', async () => {
+    const x = await promiseFinite ( 1000, 'expired', (resolve,reject) => {
                         resolve('success')
                     })
-              .then ( x => {
-                                expect (x).to.be.equal ('success', 'Bluebird promise is not resolved');
-                                done()
-                    })
-            .catch ( err => {
-                                console.log (err)
-                                done()
-                            })
+    expect (x).toBe ('success')
  }) // it resolved
 
 
 
-it ( 'Promise is timeouted', done => {
-    promiseFinite ( 500, 'timeout', (resolve, reject) => {
+it ( 'Promise is timeouted', async () => {
+    const x = await promiseFinite ( 500, 'timeout', (resolve, reject) => {
                         // ... forget to resolve should trigger the timeout
                })
-            .then ( x => {
-                    expect (x).to.be.equal ('timeout', 'Bluebird promise was not timeouted');
-                    done()
-            })
-            .catch ( err => {
-                    console.log ( err )
-                    done()
-            })
+    expect (x).toBe ('timeout')
 }) // it not resolved
 }) // describe
-
-
